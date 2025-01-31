20c2ef3e920d7ab78bda37dfe03530d4dd434a55
quinxixl <quinxixldev@gmail.com>
2025-01-31 14:41:59 +0300

    second commit

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controllers;
 
+
 class IndexController
 {
     public function showPage(): void
     {
         require_once __DIR__ . '/../../view/index.html';
+
     }
 }
---
+++ app/Database/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Database;
+
+class Database implements DatabaseInterface
+{
+
+    public function insert()
+    {
+        // TODO: Implement insert() method.
+    }
+}
