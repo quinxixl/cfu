ac8259e98ddab015eec2d67dda8f7dbc8f5f4297
quinxixl <quinxixldev@gmail.com>
2025-02-13 14:18:44 +0300

    relax controller changes

--- app/Controllers/RelaxController.php
+++ app/Controllers/RelaxController.php
@@ -9,6 +9,43 @@ use Twig\Error\SyntaxError;
 class RelaxController
 {
     private Environment $twig;
+    private array $sliderData = [
+        [
+            'date' => '01.02',
+            'image' => 'test.jpg',
+            'description' => 'левая тусовка симфа',
+            'title' => 'lb3',
+            'organizer' => 'test.org.jpg'
+        ],
+        [
+            'date'=>'05.12',
+            'image'=>'test.jpg',
+            'description'=>'главная тусовка симфа',
+            'title'=>'lb4',
+            'organizer' => 'test.org.jpg'
+        ],
+        [
+            'date'=>'08.11',
+            'image'=>'test.jpg',
+            'description'=>'правая тусовка симфа',
+            'title'=>'lb5',
+            'organizer' => 'test.org.jpg'
+        ],
+        [
+            'date'=>'08.12',
+            'image'=>'test.jpg',
+            'description'=>'чето там',
+            'title'=>'lb6',
+            'organizer' => 'test.org.jpg'
+        ],
+        [
+            'date'=>'03.12',
+            'image'=>'test.jpg',
+            'description'=>'еще чето там',
+            'title'=>'lb6',
+            'organizer' => 'test.org.jpg'
+        ]
+    ];
 
     public function __construct(Environment $twig)
     {
@@ -22,34 +59,6 @@ class RelaxController
      */
     public function showPage(): void
     {
-        echo $this->twig->render('relax.html.twig', [
-            'left__card' => [
-                [
-                    'date' => '01.02',
-                    'image' => 'test.jpg',
-                    'description' => 'левая тусовка симфа',
-                    'title' => 'lb3',
-                    'organizer' => 'test.org.jpg'
-                ]
-            ],
-            'center__card' => [
-                [
-                    'date'=>'05.12',
-                    'image'=>'test.jpg',
-                    'description'=>'главная тусовка симфа',
-                    'title'=>'lb4',
-                    'organizer' => 'test.org.jpg'
-                ]
-            ],
-            'right__card' => [
-                [
-                    'date'=>'08.11',
-                    'image'=>'test.jpg',
-                    'description'=>'правая тусовка симфа',
-                    'title'=>'lb5',
-                    'organizer' => 'test.org.jpg'
-                ]
-            ]
-        ]);
+        echo $this->twig->render('relax.html.twig', [$this->sliderData]);
     }
 }
