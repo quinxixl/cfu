f6191ade2afeba60d1eb748f0aa9628bde0ea7fd
Артём <dhdb82@inbox.ru>
2025-02-25 10:17:38 +0300

    relax

--- public/src/js/slider.js
+++ public/src/js/slider.js
@@ -1,14 +1,57 @@
-const allSlides = [
+document.addEventListener('DOMContentLoaded', function (){
+    if(window.sliderData){
+        const sliderContainer=document.getElementById('slider')
 
-]
-const nextSlide = document.querySelector('.main__slider__btn')
-const previousSlide = document.querySelector('.main__slider__btn__2')
+        sliderData.forEach(item =>{
+            const slide = document.createElement('div')
+            slide.classList.add('swiper-slide')
+            slide.innerHTML=`
+                    <div class="main__card-wrapper">
+                        <p class="main__card-date">${ item.date }</p>
+                        <div class="main__card-div">
+                            <img src="/src/image/${ item.image }" alt="#" class="main__card-image">
+                            <p class="main__card-description">${ item.description }</p>
+                        </div>
+                        <a class ="main__card-btn" href="#">Подробнее</a>
+                        <p class="main__card-title">${ item.title }</p>
+                        <img src="/src/image/${ item.organizer }" alt="#" class="main__card-organizer">
+                    </div>
+                `
+            sliderContainer.appendChild(slide)
+        })
 
-let activeSlide = 0;
+        const swiper = new Swiper('.slider-wrapper',{
+            spaceBetween: 10,
+            loop: true,
+            centeredSlides: false,
+            grabCursor: true,
+            navigation:{
+                nextEl: '.main__slider__btn__2',
+                prevEl: '.main__slider__btn',
+            },
+            slideToClickedSlide: true,
 
-const newSlideGeneration = () =>{
-    let newSlide = activeSlide + 1;
-    if(newSlide >= allSlides.length) activeSlide = 0;
-}
+            speed:600,
+            breakpoints:{
+                0:{
+                    slidesPerView:1,
+                },
+                620:{
+                    slidesPerView:2,
+                },
+                1024:{
+                    slidesPerView:3,
+                },
+            },
+        })
+    }
+
+
+});
+
+
+
+
+
 
 
