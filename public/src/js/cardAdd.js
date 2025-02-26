cde4db851dc08ef29fede78ceb3d3d6728302f44
Артём <dhdb82@inbox.ru>
2025-02-26 10:35:40 +0300

    new

--- public/src/js/cardAdd.js
+++ public/src/js/cardAdd.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     if (window.cardData) {
         const cardContainer = document.getElementById('card__wrapper')
 
-        sliderData.forEach(element => {
-            const card = document.createElement('a')
-            card.classList.add('card__href')
+        sliderData.forEach(item => {
+            const card = document.createElement('div')
+            card.classList.add('card__wrapper-2')
             card.innerHTML = `
+                <a href="${item.href}" class="card__href">
                     <div class="card">
-                        <img src="/src/image/{{ elemnt.image }}" alt="" class="card__img">
-                        <p class="card__name">{{element.name}}</p>
-                        <img src="/src/image/{{ elemnt.logo }}" alt="" class="card__logo">
-                    </div>
+                        <img src="/src/image/${ item.image }" alt="" class="card__img">
+                        <p class="card__name">${item.name}</p>
+                        <img src="/src/image/${ item.logo }" alt="" class="card__logo">
+                    </div>    
+                </a>
                 `
             cardContainer.appendChild(card)
         })
