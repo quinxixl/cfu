2e024129ce26b160be75012f7643ec3e8b85ca36
quinxixl <quinxixldev@gmail.com>
2025-02-27 14:07:22 +0300

    relax controller edit

--- app/Controllers/RelaxController.php
+++ app/Controllers/RelaxController.php
@@ -9,43 +9,7 @@ use Twig\Error\SyntaxError;
 class RelaxController
 {
     private Environment $twig;
-    private array $sliderData = [
-        [
-            'date' => '01.02',
-            'image' => 'test.jpg',
-            'description' => 'левая тусовка симфа',
-            'title' => 'lb3',
-            'organizer' => 'test.org.jpg'
-        ],
-        [
-            'date'=>'05.12',
-            'image'=>'test.jpg',
-            'description'=>'главная тусовка симфа',
-            'title'=>'lb4',
-            'organizer' => 'test.org.jpg'
-        ],
-        [
-            'date'=>'08.11',
-            'image'=>'test.jpg',
-            'description'=>'правая тусовка симфа',
-            'title'=>'lb5',
-            'organizer' => 'test.org.jpg'
-        ],
-        [
-            'date'=>'08.12',
-            'image'=>'test.jpg',
-            'description'=>'чето там',
-            'title'=>'lb6',
-            'organizer' => 'test.org.jpg'
-        ],
-        [
-            'date'=>'03.12',
-            'image'=>'test.jpg',
-            'description'=>'еще чето там',
-            'title'=>'lb6',
-            'organizer' => 'test.org.jpg'
-        ]
-    ];
+
 
     public function __construct(Environment $twig)
     {
@@ -59,6 +23,103 @@ class RelaxController
      */
     public function showPage(): void
     {
-        echo $this->twig->render('relax.html.twig', ['sliderData' => $this->sliderData]);
+        echo $this->twig->render('relax.html.twig', ['sliderData' => $this->getSliderData(), 'cardData' => $this->getCardData()]);
+    }
+
+    private function getSliderData(): array
+    {
+        return [
+            [
+                'date' => '01.02',
+                'image' => 'test.jpg',
+                'description' => 'левая тусовка симфа',
+                'title' => 'lb3',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'date'=>'05.12',
+                'image'=>'test.jpg',
+                'description'=>'главная тусовка симфа',
+                'title'=>'lb4',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'date'=>'08.11',
+                'image'=>'test.jpg',
+                'description'=>'правая тусовка симфа',
+                'title'=>'lb5',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'date'=>'08.12',
+                'image'=>'test.jpg',
+                'description'=>'чето там',
+                'title'=>'lb6',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'date'=>'03.12',
+                'image'=>'test.jpg',
+                'description'=>'еще чето там',
+                'title'=>'lb6',
+                'organizer' => 'test.org.jpg'
+            ]
+        ];
+    }
+
+    private function getCardData(): array
+    {
+        return [
+            [
+                'image' => 'test.jpg',
+                'title' => 'lb1',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb2',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb3',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb4',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb5',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb6',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image' => 'test.jpg',
+                'title'=>'lb7',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb8',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image'=>'test.jpg',
+                'title'=>'lb9',
+                'organizer' => 'test.org.jpg'
+            ],
+            [
+                'image' => 'test.jpg',
+                'title'=>'lb10',
+                'organizer' => 'test.org.jpg'
+            ],
+        ];
     }
 }
